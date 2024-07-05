da846d9f2ee9a5c1e564d8a47bb1afcb363a181c
azufb <curiousazunas@gmail.com>
2024-07-05 21:52:07 +0900

    scriptの場所変更

--- src/index.tsx
+++ src/index.tsx
@@ -13,17 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/serviceWorker.js", { scope: "/" })
-    .then((registration) => {
-      console.log("Service Worker Registered");
-    });
-  navigator.serviceWorker.ready.then((registration) => {
-    console.log("Service Worker Ready!");
-  });
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
